dbdd105757958f61fe66ee8b991f5a94784d190c
artumont <197291181+artumont@users.noreply.github.com>
2025-02-02 20:15:24 -0600

    Add custom cursor styles and improve profile section layout with separators

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ function performAnimation() {
     });
 }
 
+function openWindow(string) {
+    window.open("https://" + string, '_blank');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('bg-video').playbackRate = 0.75;
+    document.getElementById('pfp-video').playbackRate = 0.75;
     performAnimation();
 });
