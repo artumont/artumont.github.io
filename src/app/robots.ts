115ea6544b2981abfde447caf360d96567694165
artumont <197291181+artumont@users.noreply.github.com>
2026-07-20 21:04:59 -0600

    fix(seo): statically export robots route

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,13 +2,15 @@ import type { MetadataRoute } from 'next';
 
 const SITE_URL = 'https://artumont.online';
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
-        sitemap: `${SITE_URL}/sitemap.xml`,
-        host: SITE_URL,
-    };
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
 }
